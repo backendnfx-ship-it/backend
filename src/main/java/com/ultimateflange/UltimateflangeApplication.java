75ce2654ad1a8b17425b7803d7738b0c7b9556d5
backendnfx-ship-it <backendnfx@gmail.com>
2026-03-11 14:52:46 +0530

    Update UltimateflangeApplication.java

--- src/main/java/com/ultimateflange/UltimateflangeApplication.java
+++ src/main/java/com/ultimateflange/UltimateflangeApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAsync  // 👈 ADD THIS
 public class UltimateflangeApplication {
 
 	public static void main(String[] args) {
